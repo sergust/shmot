245da31382753a905d17192b7b7e1807495af7c9
Sergey Ustinov <ustinov.biz@gmail.com>
2019-12-06 11:04:36 +0300

    remove: app.js remained seeder code

--- src/App.js
+++ src/App.js
@@ -32,10 +32,6 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
-      addCollectionAndDocuments(
-        'collections',
-        collectionsArray.map(({ title, items }) => ({ title, items }))
-      );
     });
   }
 
