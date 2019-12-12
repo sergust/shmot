83fee16f974df40a97d3e0ab1d7cf524322c995f
Sergey Ustinov <ustinov.biz@gmail.com>
2019-12-12 11:04:14 +0300

    add: payment request to back end

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_lzerTcuNC4Y76z6huHbXboag00lmhdlHzk';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.error('Payment error', error);
+        alert('Issue with payment');
+      });
   };
 
   return (
